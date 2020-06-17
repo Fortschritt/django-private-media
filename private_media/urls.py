e2ab0e52ad4f0eae9824ebfa20ad0d93cd4fafd2
Mattes Sarcander <info@fortschritt.pro>
2020-06-17 16:08:18 +0200

    update urls for Django 2.2 compatibility

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import patterns, url
 from django.conf import settings
+from django.conf.urls import url
+from private_media.views import serve_private_file
 
-urlpatterns = patterns('private_media.views',
-    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), 'serve_private_file',),
-)
+app_name = 'private_media'
+urlpatterns = [
+    url(
+        r'^{0}(?P<path>.*)$'.format(
+            settings.PRIVATE_MEDIA_URL.lstrip('/')
+        ),
+        serve_private_file,
+    ),
+]
